c5a6022a7dd04bd2ec4c6e4f9a500ca60611309a
HeineKayn <61800617+HeineKayn@users.noreply.github.com>
2022-12-23 02:05:50 +0100

    update site en direct

--- rphyst/static/js/combat.js
+++ rphyst/static/js/combat.js
@@ -1,16 +1,33 @@
+var players = []
+
 function updateGame(){
-    $.post('/rphyst/combat/update'
-        ).done(function(response) {
-            console.log(response["content"])
+    $.post('/rphyst/combat/update').done(function(response) {
+            tours.innerHTML = response["content"]["tour"];
+            for(var player of response["content"]["joueurs"]){
+                if (!players.includes(player["nom"])){
+                    addPlayer(player);
+                }
+            }
     });
 }
 
 // Si on a demandé à update la partie et qu'un joueur n'est pas dans ceux affichés
-function addPlayer(name) {
-    
+function addPlayer(player) {
+    var clone = templatePlayer.cloneNode(true);
+    clone.classList.remove("template");
+    clone.children[0].children[1].innerHTML = player["init"];
+    // clone.children[1]
+    clone.children[2].children[0].children[0].style.width = Math.round(player["hp"] / player["stats"]["HP"] * 100) + "%";
+    clone.children[2].children[0].children[0].innerHTML = player["hp"];
+    players.push(player["nom"]);
+    templatePlayer.after(clone);
 }
 
+var tours = document.getElementById("valueTour");
+var templates = document.getElementsByClassName("template");
+var templatePlayer = templates[0];
+var templateSpells = templates[1];
 
 document.addEventListener("DOMContentLoaded", function() {
     setInterval(updateGame,500);
-  });
+});
